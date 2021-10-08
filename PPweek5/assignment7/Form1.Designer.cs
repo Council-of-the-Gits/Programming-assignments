1cbbb450a1f2d0e5a7f27d4a28f00e439b4f5c5d
l-huisman <64755533+l-huisman@users.noreply.github.com>
2021-10-08 20:12:45 +0200

    Full assignments
    5 missing was optional

--- PPweek5/assignment7/Form1.Designer.cs
+++ PPweek5/assignment7/Form1.Designer.cs
@@ -29,20 +29,46 @@ namespace assignment7
         /// </summary>
         private void InitializeComponent()
         {
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.throwButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.AutoSize = true;
+            this.outputLabel.Location = new System.Drawing.Point(55, 44);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(0, 15);
+            this.outputLabel.TabIndex = 0;
+            // 
+            // throwButton
+            // 
+            this.throwButton.Location = new System.Drawing.Point(133, 181);
+            this.throwButton.Name = "throwButton";
+            this.throwButton.Size = new System.Drawing.Size(111, 35);
+            this.throwButton.TabIndex = 1;
+            this.throwButton.Text = "Throw";
+            this.throwButton.UseVisualStyleBackColor = true;
+            this.throwButton.Click += new System.EventHandler(this.throwButton_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(404, 461);
+            this.ClientSize = new System.Drawing.Size(384, 261);
+            this.Controls.Add(this.throwButton);
+            this.Controls.Add(this.outputLabel);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.Button throwButton;
     }
 }
 
---
+++ PPweek5/assignment7/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace assignment7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void throwButton_Click(object sender, EventArgs e)
+        {
+            outputLabel.Text = null;
+            
+            Random random = new Random();
+
+            int randomNumber;
+
+            int counter = 1;
+
+            int[] dice = {0,0,0,0,0,0};
+
+
+
+            for (int i = 0; i < 6000; i++)
+            {
+                randomNumber = random.Next(1, 7);
+
+                switch (randomNumber)
+                {
+                    case 1:
+                        dice[0]++;
+                        break;
+                    case 2:
+                        dice[1]++;
+                        break;
+                    case 3:
+                        dice[2]++;
+                        break;
+                    case 4:
+                        dice[3]++;
+                        break;
+                    case 5:
+                        dice[4]++;
+                        break;
+                    case 6:
+                        dice[5]++;
+                        break;
+                }
+            }
+
+            for (int i = 0; i < 6; i++)
+            {
+                outputLabel.Text += $"Number of throws of value {counter} = {dice[i]}\n";
+
+                counter++;
+            }
+
+        }
+    }
+}
